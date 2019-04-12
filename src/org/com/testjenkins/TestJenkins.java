534ef71f9d4585e69116546d4219191a11c9a415
sunils <sunil.solanki@sigma-systems.com>
2019-04-12 15:49:24 +0530

    initiate github project for jenkins test

---
+++ src/org/com/testjenkins/TestJenkins.java
@@ -0,0 +1,10 @@
+package org.com.testjenkins;
+
+public class TestJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
