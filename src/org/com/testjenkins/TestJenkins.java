79d62a3548f2dfd74e0b97aae7776a60c28f4057
sunils <sunil.solanki@sigma-systems.com>
2019-04-15 16:21:40 +0530

    github webhook updated

--- src/org/com/testjenkins/TestJenkins.java
+++ src/org/com/testjenkins/TestJenkins.java
@@ -5,7 +5,6 @@ public class TestJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, the change i am making");
-		System.out.print("one more line dvds");
 	}
 
 }
