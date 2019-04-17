f7e4df50c71137202e14229d09a6bc4f5cab2503
sunils <sunil.solanki@sigma-systems.com>
2019-04-17 12:08:12 +0530

    converted project to spring boot

---
+++ src/main/java/com/jenkinstest/CourserApiApp.java
@@ -0,0 +1,13 @@
+package com.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourserApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourserApiApp.class , args);
+	}
+
+}
